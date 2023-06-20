9c8fc8512481a4adfd44f5dda68b1fd0b5d3124e
RicckGomes <richardgomesteixeira@yahoo.com.br>
2023-06-19 22:00:22 -0300

    introdução e manipulação de strings com python

---
+++ Python/strings.py
@@ -0,0 +1,15 @@
+# Exemplo de programa que aboda o tipo de dado string em Python
+
+frase = "Olá mundo!";
+# Declaração de uma String
+print("String original: ");
+print(frase);
+print();
+
+# Acessando caracteres individuais
+print("Acessando caracteres individuais: ");
+print("Primeiro caractere: ", frase[0]);
+print();
+
+print("Acessando caracteres individuais: ");
+print("Ultimo caractere: ", frase[-1]);
