484838b3ebdb00cee27953c4ea2f4f34daf9d733
RicckGomes <richardgomesteixeira@yahoo.com.br>
2023-06-29 19:46:38 -0300

    Primeiros exemplos operadores aritméticos

---
+++ Python/Estruturas-operadores/operadoresAritmeticos.py
@@ -0,0 +1,23 @@
+"""
+Operadores Aritméticos
+"""
+
+# Criação de variáveis
+a = 10;
+b = 3;
+
+# Somando valores
+soma = a + b;
+subtracao = a - b;
+multiplicacao = a * b;
+divisao = a / b;
+resto = a % b;
+descarteParteFracionaria = a // b;
+potencia = a ** b;
+print("A soma dos números é:", soma);
+print("A subtração dos números é:", subtracao);
+print("A multiplicação dos números é:", multiplicacao);
+print("A divisão dos números é:", divisao);
+print("A potenciação dos números é:", potencia);
+print("O resto da divisão é:", resto);
+print("Divisão após descartar parte fracionária:", descarteParteFracionaria);
