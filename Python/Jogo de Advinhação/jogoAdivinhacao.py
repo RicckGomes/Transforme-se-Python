e5e80d91c856ac111fd7974efa254f3244936b64
RicckGomes <richardgomesteixeira@yahoo.com.br>
2023-07-11 21:44:06 -0300

    Conclusão do jogo da Adivinhação

--- Python/Jogo de Advinhação/jogoAdivinhacao.py
+++ Python/Jogo de Advinhação/jogoAdivinhacao.py
@@ -1,22 +1,71 @@
 import random
 
-def adivinhacao():
-    numero_secreto = random.randint(1, 100)  # Gera um número aleatório entre 1 e 100
-    tentativas = 0
+# Criei a função abaixo para obrigar que o usuário digite um valor válido
+def obterInputInteiro(mensagem):
+    while True:
+        try:
+            valor = int(input(mensagem));
+            return valor;
+        except ValueError:
+            print("Valor inválido. Digite um número inteiro.");
+            print();
 
-    print("Bem-vindo ao jogo de adivinhação de números!")
-    print("Estou pensando em um número entre 1 e 100.")
+def adivinhacao():
+    print("----------------------------------------------------------");
+    print(" Antes de começarmos, vamos definir o limite dos números.");
+    print(" Escolha o valor máximo para o número secreto.");
+    print("----------------------------------------------------------");
+    
+    print();
+    limite = obterInputInteiro("Digite o valor máximo desejado: ");
+    print();
+    numeroSecreto = random.randint(1, limite);
+    tentativas = 0;
+    print();
+
+    print("====================================================");
+    print(" Bem-vindo ao jogo de adivinhação!");
+    print(" Neste jogo, você deve adivinhar um número secreto.");
+    print(f" Estou pensando em um número entre 1 e {limite}.")
+    print(" Você deverá escolher a quantidade de tentativas.");
+    print(" Boa sorte!!!");
+    print("====================================================");
+
+    escolhaTentativas = obterInputInteiro("Infome a quantidade de tentativas: ");
+    print("==================================================");
 
     while True:
-        palpite = int(input("Digite o seu palpite: "))
-        tentativas += 1
-
-        if palpite < numero_secreto:
-            print("Tente um número maior!")
-        elif palpite > numero_secreto:
-            print("Tente um número menor!")
+        tentativas += 1;
+        print(f"Tentativa {tentativas}!");
+        palpite = obterInputInteiro("Digite o seu palpite: ");
+        
+        if palpite < numeroSecreto:
+            if tentativas == escolhaTentativas:
+                print("=========================================================");
+                print(f"Não foi dessa vez, você atingiu o limite de {escolhaTentativas} tentativas!");
+                print("=========================================================");
+                break;
+            print("Tente um número maior!");
+            print("=============================================");
+        elif palpite > numeroSecreto:
+            if tentativas == escolhaTentativas:
+                print("=========================================================");
+                print(f"Não foi dessa vez, você atingiu o limite de {escolhaTentativas} tentativas!");
+                print("=========================================================");
+                break;
+            print("Tente um número menor!");
+            print("=============================================");
         else:
-            print(f"Parabéns! Você acertou o número em {tentativas} tentativas!")
-            break
-
-adivinhacao()
+            print(f"Parabéns! Você acertou o número em {tentativas} tentativas!");
+            print("=============================================");
+            break;
+    print();
+    
+    jogarNovamente = obterInputInteiro("Deseja jogar novamente? 1. SIM - 2. NÃO: ");  
+    if jogarNovamente == 1:
+        adivinhacao();
+    # O código abaixo também funcionaria, porém por questão de desempenho vou com o IF
+    # while jogarNovamente == 1:
+    #     adivinhacao();
+    
+adivinhacao();
